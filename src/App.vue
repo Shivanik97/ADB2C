3d08158f1454944dc6a7086cf44e556e43f67b26
Shivani K <shivanik.bbi18@rvce.edu.in>
2023-04-04 10:49:13 +0530

    vue app with adb2c authentication

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <button v-if="isLoggedIn" @click="logOut">Log Out</button>
+  <button v-else @click="logIn">Log In</button>
 </template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+import {msalInstance}from "./services/msal";
+import {AuthError} from "@azure/msal-browser";
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {
+  isLoggedIn = false;
+  async logIn() {
+    try {
+      await msalInstance.loginPopup();
+      this.isLoggedIn = true;
+    } catch (err) {
+      const authErr = err as AuthError;
+      if (authErr) {
+        if (authErr.errorCode == "user_cancelled") {
+          return;
+        }
+      }
+      console.error({err});
+    }
+  }
+  async logOut() {
+    await msalInstance.logoutPopup();
+    this.isLoggedIn = false;
+  }
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +43,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
---
+++ src/services/msal.ts
@@ -0,0 +1,55 @@
+import {
+  BrowserCacheLocation,
+  Configuration,
+  LogLevel,
+  PublicClientApplication,
+} from "@azure/msal-browser";
+
+const msalConfig: Configuration = {
+  auth: {
+    clientId: 'VUE_APP_MSAL_CLIENT_ID',
+    authority:
+      "https://ariqtlearning.b2clogin.com/ariqtlearning.onmicrosoft.com/B2C_1_signInandSignUp",
+    knownAuthorities: ["ariqtlearning.b2clogin.com"],
+    redirectUri: "http://localhost:8080",
+    postLogoutRedirectUri: "http://localhost:8080",
+  },
+  cache: {
+    cacheLocation: BrowserCacheLocation.LocalStorage,
+    storeAuthStateInCookie: false,
+    secureCookies: false,
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            break;
+
+          case LogLevel.Info:
+            console.info(message);
+            break;
+
+          case LogLevel.Verbose:
+            console.debug(message);
+            break;
+
+          case LogLevel.Warning:
+            console.warn(message);
+            break;
+
+          default:
+            break;
+        }
+      },
+      piiLoggingEnabled: false,
+    },
+  },
+};
+
+export const msalInstance = new PublicClientApplication(msalConfig);
